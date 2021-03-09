804448f44b9e3794c0a6b431c6bd8520674e8e35
As4s1n <behera.sarthak3@gmail.com>
2021-03-09 21:39:11 +0530

    Done Kalman Filter ellipse part is incomplete

--- Q2/B/b_kf.py
+++ Q2/B/b_kf.py
@@ -1,12 +1,11 @@
+from matplotlib import colors
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 from numpy.lib.function_base import interp
 import math
 import matplotlib.markers as markers
-
-bf_sz = 30
-sensor_cred = 0.7       # Less is better. Lesser this value, more sure we are about the sensor measurement.
+import matplotlib.patches as pat
 
 class Airplane:
     def __init__(self, s):
@@ -45,23 +44,32 @@ def observation_model(S, C, delta):
 def chebyshev(a, b):
     return max(abs(a[0] - b[0]), abs(a[1] - b[1]))
 
-def sensor_model(z, x):
-    return math.pow(sensor_cred, chebyshev(z, x))
+def kalman_filter(mu, sigma, z, A, U, B, R, C, Q):
+    mu_t_dash = A.dot(mu) + B.dot(U)
+    sigma_t_dash = A.dot(sigma).dot(A.T) + R
 
-def bayes_filter(Bel, TM, obs):
-    Bel_dash = TM.dot(Bel)
-    sensor_pref = np.zeros((bf_sz*bf_sz, 1))
+    K_t = sigma_t_dash.dot(C.T).dot(np.linalg.pinv(C.dot(sigma_t_dash).dot(C.T) + Q))
+    mu_t = mu_t_dash + K_t.dot(z - C.dot(mu_t_dash))
+    sigma_t = (np.eye(4, 4) - K_t.dot(C)).dot(sigma_t_dash)
+    return mu_t, sigma_t
 
-    for k in range(bf_sz*bf_sz):
-        i, j = k//bf_sz, k%bf_sz
-        sensor_pref[k, 0] = sensor_model(obs, (i, j))
-    
-    Bel_dash = np.multiply(Bel_dash, sensor_pref)
-    eta = np.sum(Bel_dash)
-    Bel_new = Bel_dash/eta
-    return Bel_new
+def _plot_gaussian(mean, covariance, color='k', zorder=0):
+    """Plots the mean and 2-std ellipse of a given Gaussian"""
+    # plt.plot(mean[0], mean[1], color[0] + ".", zorder=zorder)
 
+    if covariance.ndim == 1:
+        covariance = np.diag(covariance)
 
+    radius = np.sqrt(5.991)
+    eigvals, eigvecs = np.linalg.eig(covariance)
+    axis = np.sqrt(eigvals) * radius
+    slope = eigvecs[1][0] / eigvecs[1][1]
+    angle = 180.0 * np.arctan(slope) / np.pi
+
+    return pat.Ellipse(
+        mean, axis[0], axis[1], angle=angle,
+        fill=False, color=color, linewidth=1, zorder=zorder
+    )
 
 U = np.zeros((2, 1))
 C = np.eye(2, 4)
@@ -106,33 +114,6 @@ my_airplane.observed(observation_model(my_airplane.s, C, np.random.multivariate_
 T = 20
 delta_t = 1
 
-Bel = np.zeros((T, bf_sz*bf_sz, 1))
-Bel[0, int(init_state[0, 0]*bf_sz + init_state[1, 0]), 0] = 1
-
-TM = np.zeros((bf_sz*bf_sz, bf_sz*bf_sz))       # Transition Matrix
-
-for i in range(bf_sz - 2):
-    for j in range(bf_sz - 2):
-        fr = bf_sz*i + j
-        to1, to2, to3, to4, to5, to6, to7, to8, to9 = fr + bf_sz + 1, fr + 2*bf_sz + 1, fr + bf_sz + 2, fr + 1, fr + bf_sz, fr + 2*bf_sz + 2, fr + 2*bf_sz, fr, fr + 2
-        TM[to1, fr] = 0.7
-        TM[to2, fr], TM[to3, fr], TM[to4, fr], TM[to5, fr] = 0.05, 0.05, 0.05, 0.05
-        TM[to6, fr], TM[to7, fr], TM[to8, fr], TM[to9, fr] = 0.025, 0.025, 0.025, 0.025
-
-for i in range(bf_sz - 1):
-    fr1 = bf_sz*i + bf_sz-2
-    to1 = fr1 + bf_sz + 1
-    TM[to1, fr1] = 1
-    fr2 = bf_sz*(bf_sz-2) + i
-    to2 = fr2 + bf_sz + 1
-    TM[to2, fr2] = 1
-
-for i in range(bf_sz):
-    fr1 = bf_sz*i + bf_sz-1
-    TM[fr1, fr1] = 1
-    fr2 = bf_sz*(bf_sz-1) + i
-    TM[fr2, fr2] = 1
-
 for t in range(T):
     S = motion_model(my_airplane.s, A(delta_t), B(delta_t), U, np.random.multivariate_normal(np.zeros(4,), R).reshape(4, 1))
     my_airplane.transition(S)
@@ -143,35 +124,47 @@ y_motion = [my_airplane.state_history[j][1, 0] for j in range(T)]
 x_obs = [my_airplane.observation[j][0, 0] for j in range(T)]
 y_obs = [my_airplane.observation[j][1, 0] for j in range(T)]
 
-for i in range(1, 20):
-    Bel[i] = bayes_filter(Bel[i-1], TM, (x_obs[i], y_obs[i]))
+mu_0 = init_state
+sigma_0 = 0.01 * np.eye(4, 4)
 
-predicted_path = []
+Bel = [(mu_0, sigma_0)]
 
-for i in range(20):
-    k = np.argmax(Bel[i])
-    x, y = k//bf_sz, k%bf_sz
-    predicted_path.append((x, y))
+for t in range(T):
+    mu, sigma = kalman_filter(Bel[t][0], Bel[t][1], np.array([[x_obs[t]], [y_obs[t]]]), A(delta_t), U, B(delta_t), R, C, Q)
+    Bel.append((mu, sigma))
+
+predicted_state = [Bel[i][0] for i in range(len(Bel))]
+predicted_state = np.array(predicted_state)
 
 fig, ax = plt.subplots(1, 1, figsize = (10, 10))
-ax.set_xticks(np.arange(0, bf_sz+1, 1))
-ax.set_yticks(np.arange(0, bf_sz+1, 1))
-ax.set_xlim([-0.5, bf_sz-0.5])
-ax.set_ylim([-0.5, bf_sz-0.5])
+ax.set_xticks(np.arange(0, 50, 1))
+ax.set_yticks(np.arange(0, 50, 1))
+ax.set_xlim([0, 50])
+ax.set_ylim([0, 50])
+ell = [_plot_gaussian(mu_0[:, 0], sigma_0)]
+
+for t in range(1, 20):
+    ell.append(_plot_gaussian(Bel[t][0][:, 0], Bel[t][1]))
+
+e = ell[0]
+ax.add_patch(e)
 line1, = ax.plot(x_motion, y_motion, color='g')
 line2, = ax.plot(x_obs, y_obs, color='r')
+line3, = ax.plot(predicted_state[0, 0, 0], predicted_state[0, 1, 0], color='b')
 
-P = np.arange(bf_sz)
-all_points = np.dstack(np.meshgrid(P, P)).reshape(-1, 2)
-marker = markers.MarkerStyle(marker='s')
+# P = np.arange(bf_sz)
+# all_points = np.dstack(np.meshgrid(P, P)).reshape(-1, 2)
+# marker = markers.MarkerStyle(marker='s')
 
-scat3 = plt.scatter(all_points[:, 0], all_points[:, 1], c=Bel[0, :, 0]*1000, s=200, cmap='Greys', edgecolors='k', marker=marker)
+# scat3 = plt.scatter(all_points[:, 0], all_points[:, 1], c=Bel[0, :, 0]*1000, s=200, cmap='Greys', edgecolors='k', marker=marker)
 scat1 = plt.scatter([x_motion[0]], [y_motion[0]], c='g', s=50, edgecolors='k')
 scat2 = plt.scatter([x_obs[0]], [y_obs[0]], c='r', s=50, edgecolors='k')
-scat4 = plt.scatter([10], [10], c='b', s=200, edgecolors='k', marker=marker)
+scat3 = plt.scatter([predicted_state[0, 0, 0]], [predicted_state[0, 1, 0]], c='b', s=50, edgecolors='k')
+# scat4 = plt.scatter([10], [10], c='b', s=200, edgecolors='k', marker=marker)
 
 line1.set_label('Actual motion')
 line2.set_label('Observed motion')
+line3.set_label('Predicted motion')
 plt.grid()
 plt.legend(loc="upper left")
 
@@ -179,22 +172,28 @@ def animate(i):
     arr = []
     brr = []
     shade_map = []
-    for a in range(bf_sz):
-        for b in range(bf_sz):
-            arr.append(a)
-            brr.append(b)
-            shade_map.append(Bel[i,a*bf_sz + b,0]*1000)
+    # for a in range(bf_sz):
+    #     for b in range(bf_sz):
+    #         arr.append(a)
+    #         brr.append(b)
+    #         shade_map.append(Bel[i,a*bf_sz + b,0]*1000)
 
-    scat3.set_offsets(np.c_[arr, brr])
-    scat3.set_array(np.array(shade_map))
+    # scat3.set_offsets(np.c_[arr, brr])
+    # scat3.set_array(np.array(shade_map))
     line1.set_data(x_motion[:i+1], y_motion[:i+1])
     line2.set_data(x_obs[:i+1], y_obs[:i+1])
+    line3.set_data(predicted_state[:i+1, 0, 0], predicted_state[:i+1, 1, 0])
     scat1.set_offsets(np.c_[[x_motion[i]], [y_motion[i]]])
     scat2.set_offsets(np.c_[[x_obs[i]], [y_obs[i]]])
-    scat4.set_offsets(np.c_[[predicted_path[i][0]], [predicted_path[i][1]]])
-    return scat3, scat4, scat1, scat2, line1, line2,
+    scat3.set_offsets(np.c_[[predicted_state[i, 0, 0]], [predicted_state[i, 1, 0]]])
+    e.set_angle(ell[i].angle)
+    e.set_center(ell[i].center)
+    e.set_width(ell[i].get_width())
+    e.set_height(ell[i].get_height())
+    # scat4.set_offsets(np.c_[[predicted_path[i][0]], [predicted_path[i][1]]])
+    return line1, line2, line3, scat1, scat2, scat3, e, 
 
 ani = animation.FuncAnimation(fig, animate, T, interval=delta_t * 1000, blit=True)
-ani.save('b.mp4')
+ani.save('b_kf.mp4')
 
 plt.show()
